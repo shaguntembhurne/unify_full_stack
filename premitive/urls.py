6d37b67a6122bdcf0fb7b4b97ab74c4f3c59408d
shaguntembhurne <shaxtembhurne@gmail.com>
2025-09-18 08:49:17 +0530

    Add Calendar page route, navbar link, and template refactor

--- premitive/urls.py
+++ premitive/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from django.contrib.auth.views import LogoutView
 from . import views
 
@@ -12,6 +12,8 @@ urlpatterns = [
     path('profile/update/', views.profile_update, name='profile_update'),
     path('notifications/dropdown/', views.notifications_dropdown, name='notifications_dropdown'),
     path('notifications/mark-read/', views.notifications_mark_read, name='notifications_mark_read'),
+    path('calendar/', TemplateView.as_view(template_name='unify_calender.html'), name='calendar'),
+    path('analytics/teachers/', TemplateView.as_view(template_name='teachers_analytics.html'), name='teachers_analytics'),
     path('signup/', RedirectView.as_view(pattern_name='premitive:auth', permanent=False), name='signup'),
     path('login/', RedirectView.as_view(pattern_name='premitive:auth', permanent=False), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
